e8616f1285bc7a96da13572a201d7289c7f0aa0d
KarpenkoIlya <90862395+KarpenkoIlya@users.noreply.github.com>
2021-09-16 22:08:09 +0300

    Add files via upload

---
+++ L4/Lr_4/Lr_4/Lr_4.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+int check(int, char);
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    char s[100];
+    int d;
+    cout << "Для завершения текста написать .\n";
+    cin.getline(s, 100, '.');
+    for (int i = 0; i < 100; i++) {
+        for (int n = 1; n < i; n++) {
+            if (s[i] == s[i - n]) goto Mark;
+        }
+        cout << s[i];
+        Mark: continue;
+    }
+    return 0;
+}
